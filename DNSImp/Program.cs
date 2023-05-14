432a9ea3b0a362f5f11d9763bbc8f8b917f114e4
John Gibson <jg20019@gmail.com>
2023-05-14 19:10:59 -0400

    feat: Test DNS code by using hex string for query

--- DNSImp/Program.cs
+++ DNSImp/Program.cs
@@ -1,18 +1,21 @@
 ﻿using DNSImp;
+using System.Net;
+using System.Net.Sockets;
 
-// From the example in Part 1
-var header = new DNSHeader(0x1314, 0, 1, 0, 0, 0);
-var bytes = header.ToBytes();
-BitUtils.WriteBytes(bytes);
+// var query = DNSQuery.BuildQuery("www.example.com", DNSQuery.TYPE_A);
+// Console.WriteLine(Convert.ToHexString(query));
+
+// Hex String representing a proper query
+// This was done to test that the DNS code below is working correctly. 
+// Verified that I get a correct response in Wireshark
+var query = "82980100000100000000000003777777076578616d706c6503636f6d0000010001";
+UdpClient udpClient = new UdpClient();
+IPEndPoint ep = new IPEndPoint(IPAddress.Parse("8.8.8.8"), 53);
+udpClient.Connect(ep);
+udpClient.Send(Convert.FromHexString(query));
+var response = udpClient.Receive(ref ep);
+Console.WriteLine(Convert.ToHexString(response));
+Console.Write("receive data from " + ep.ToString());
 Console.WriteLine();
 
-var question = new DNSQuestion("IN", 0x12, 0x0);
-BitUtils.WriteBytes(question.ToBytes());
-Console.WriteLine();
-
-var query = new DNSQuery("google.com");
-BitUtils.WriteBytes(query.Encode());
-Console.WriteLine();
-
-Console.WriteLine(query.Encode()[1]);
 Console.ReadKey();
