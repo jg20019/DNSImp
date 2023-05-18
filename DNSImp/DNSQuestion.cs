a77d8482759c3754b6fde4ccbebd766668bb4593
John Gibson <jg20019@gmail.com>
2023-05-18 11:47:20 -0400

    feat: Read DNSQuestion

--- DNSImp/DNSQuestion.cs
+++ DNSImp/DNSQuestion.cs
@@ -19,6 +19,32 @@ namespace DNSImp
             Class = @class;
         }
 
+        public DNSQuestion(string name, ushort type, ushort @class) 
+            : this(Encoding.ASCII.GetBytes(name), type, @class)
+        {}
+
+        public static string DecodeNameSimple(BinaryReader reader)
+        {
+            var length = (int)reader.ReadByte();
+            var parts = new List<string>();
+            var sb = new StringBuilder();
+            while (length != 0)
+            {
+                parts.Add(Encoding.ASCII.GetString(reader.ReadBytes(length)));
+                length = (int)reader.ReadByte();
+            }
+
+            return sb.AppendJoin(".", parts.ToArray()).ToString();
+        }
+        public static DNSQuestion FromReader(BinaryReader reader)
+        {
+            var name = DecodeNameSimple(reader);
+            var type = BitUtils.ToUInt16(reader.ReadBytes(2));
+            var class_ = BitUtils.ToUInt16(reader.ReadBytes(2));
+            return new DNSQuestion(name, type, class_);
+            
+        }
+
         public byte[] ToBytes()
         {
             var bytes = new List<byte>();
@@ -26,6 +52,12 @@ namespace DNSImp
             bytes.AddRange(BitUtils.GetBytes(Type));
             bytes.AddRange(BitUtils.GetBytes(Class));
             return bytes.ToArray();
+        }
+
+        public override string ToString()
+        {
+            var name = Encoding.ASCII.GetString(Name);
+            return $"DNSQuestion(name={name}, type={Type}, class={Class})";
         }
     }
 }
